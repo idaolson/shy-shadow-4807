02ea24e2432241f19ea064fc8f206eb027176103
Ida Olson <idaolson@gmail.com>
2021-09-22 09:16:21 -0600

    Create/update models

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -1,3 +1,5 @@
 class Plot < ApplicationRecord
   belongs_to :garden
+  has_many :plot_plants
+  has_many :plots, through: :plot_plants
 end
---
+++ app/models/plot_plant.rb
@@ -0,0 +1,4 @@
+class Plot < ApplicationRecord
+  belongs_to :plot
+  belongs_to :plant
+end
