6e1b176f93b57414ae5c359ed9b6fe04f1d0d33d
Ida Olson <idaolson@gmail.com>
2021-09-22 10:16:53 -0600

    Create garden plants method in garden model

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,10 @@
 class Garden < ApplicationRecord
   has_many :plots
+
+  def garden_plants
+    garden_plants = self.plots.select('plots.*, plants.name, plants.days_to_harvest')
+    .joins(:plants).where('plants.days_to_harvest < 100')
+    .pluck('plants.name')
+    garden_plants.uniq
+  end
 end
