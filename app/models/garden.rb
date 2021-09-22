8eb5aebbbfb8ec5f6e5a55723e8882a2045df495
Ida Olson <idaolson@gmail.com>
2021-09-22 10:41:33 -0600

    Complete extension

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -4,6 +4,8 @@ class Garden < ApplicationRecord
   def garden_plants
     garden_plants = self.plots.select('plots.*, plants.name, plants.days_to_harvest')
     .joins(:plants).where('plants.days_to_harvest < 100')
+    .group('plants.name')
+    .order('count(plants.name) DESC')
     .pluck('plants.name')
     garden_plants.uniq
   end
